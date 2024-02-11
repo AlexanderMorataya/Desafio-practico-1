f62d0d084533b7a2b00c3eec6b452e4ba8b11810
Diego Morataya <dieale389@gmail.com>
2024-02-10 19:23:22 -0600

    Creacion de clase Producto y Cliente, mas la lista cliente y sus metodos

--- Trabajo 1/Cliente.cs
+++ Trabajo 1/Cliente.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace Trabajo_1
 {
-    internal class Cliente
+    class Cliente
     {
+        int codigoCliente;
+        public int CodigoCliente
+        {
+            get { return codigoCliente; }
+            set { codigoCliente = value; }
+        }
+
+        string nombreCliente;
+        public string NombreCliente
+        {
+            get { return nombreCliente; }
+            set { nombreCliente = value; }
+        }
+
+        string direccion;
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        int telefono;
+        public int Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+
+        string correo;
+        public string Correo
+        {
+            get { return correo; }
+            set { correo = value; }
+        }
     }
 }
